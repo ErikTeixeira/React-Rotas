c4288cfe9023e45a52ef9f3f406681862c5307fb
ErikTeixeira <erik.barbosa515@gmail.com>
2023-09-03 21:07:17 -0300

    continuando

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import Home from './routes/Home/index.jsx'
+import Produto from './routes/Produtos/index.jsx'
+import Error from './routes/Error/index.jsx'
+import EditarProduto from './routes/EditarProdutos/index.jsx'
+
+const router = createBrowserRouter(
+  [
+    {path : "/", element : <App/>, errorElement : <Error/>,
+    
+    children:[
+      {path : "/" , element : <Home/>},
+      {path : "/produtos" , element : <Produto/>},
+      {path : "/editar/produtos/:id" , element : <EditarProduto/>},
+    ]}
+  ]
+);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
