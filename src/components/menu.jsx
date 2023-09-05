ba5df5a93edfb2550d3ebbb1fb4fe6ac15cc1e08
ErikTeixeira <erik.barbosa515@gmail.com>
2023-09-05 16:02:11 -0300

    arrumei o link

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -4,9 +4,9 @@ export default function Menu() {
 
     return(
         <nav className="menu">
-            <Link to="/">Home</Link>
+            <Link to='/'>Home</Link>
             <span> | </span>
-            <Link to="/produtos">Produtos</Link>
+            <Link to='/produtos'>Produtos</Link>
         </nav>
     )
 }
